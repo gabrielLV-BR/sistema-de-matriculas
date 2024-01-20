f0c9bcb2e461ba39cad90752d92a8fccf98db37b
Gabriel L. <gabriellovato15@gmail.com>
2024-01-20 18:46:34 -0300

    Correctly rendering Select using translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def enum_to_translated_select (hash, preffix:)
+    preffix = preffix.join(".") if preffix.kind_of? Array
+    preffix << "." if not preffix.end_with? "."
+
+    return hash
+      .map { |k, v| [k.to_s, v] }
+      .map {
+        |k, v| [I18n.t(preffix + k, default: k), k]
+      }.to_h
+  end
 end
