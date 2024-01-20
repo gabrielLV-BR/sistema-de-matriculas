1c4b35727d81b6c73e6b31783a0ad205f204e6e3
Gabriel L. <gabriellovato15@gmail.com>
2024-01-20 16:36:41 -0300

    School creation right. Added enums to fields

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -8,10 +8,22 @@ class SchoolsController < ApplicationController
   end
 
   def new
-
+    @school = School.new
   end
 
   def create
+    @school = School.new school_params
 
+    if @school.save then
+      redirect_to @school
+    else
+      redirect_back status: :unprocessable_entity, fallback_location: schools_url
+    end
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:name, :address, :telephone, :type, :regimen)
   end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,6 @@
 class School < ApplicationRecord
   has_many :classrooms
+
+  enum type: %i(kinder elementary high), _suffix: :school
+  enum regimen: %i(city state federal private), _suffix: :school
 end
